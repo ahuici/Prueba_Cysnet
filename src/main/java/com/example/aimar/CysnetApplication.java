7ef5209831006dbaba704439e7387c7dfd9b7e17
ahuici <ahuicidelg1@educacion.navarra.es>
2025-10-28 19:45:18 +0100

    Commit inicial
    Proyecto base Spring Boot

---
+++ src/main/java/com/example/aimar/CysnetApplication.java
@@ -0,0 +1,13 @@
+package com.example.aimar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CysnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CysnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aimar/CysnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aimar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CysnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
